5a5fb778adba1c245d38de6ab73bd321d4af0c00
Jsndng408 <tuh22928@temple.edu>
2021-02-27 23:15:17 -0500

    Added jsdoc module and generated sample page for documentation

--- fiscal/assets/index.js
+++ fiscal/assets/index.js
@@ -1,8 +1,22 @@
 import _ from 'lodash';
 
+/**
+ * A function that returns the contents of the base page. 
+ * @constructor
+ */
 function component() {
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'lodash'], ' ');
     return element;
 }
+
+/**
+ * Function that given a user's credentials, create a new account.
+ * @param {string} username
+ * @param {string} password
+ * @param {string} password_verify
+ * @returns {Promise} Promise object represents the status of the account
+ * @throws Will throw an error if password and password_verify are not equal
+ */
+
 document.body.appendChild(component());
