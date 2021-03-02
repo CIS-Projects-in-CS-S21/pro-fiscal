f170b1b1542262595fab3c721210b13f5f3ae1eb
Christopher-Scott <cws837@gmail.com>
2021-03-02 16:42:13 -0500

    Added stubs for ExpenseList

--- fiscal/budget_tool/views.py
+++ fiscal/budget_tool/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+class ExpenseList(APIView):
+    """
+    List all expenses, or create a new one
+    """
+
+    def get(self, request):
+        """
+        Get the list of expenses
+
+        Args:
+            request (HttpRequest): The request object from an HTTP request
+
+        Returns:
+            Response: list of expenses in JSON format
+        """
+        pass
+
+    def post(self, request):
+        """
+        Add a new expense
+
+        Args:
+            request (HttpRequest): The request object from an HTTP request
+
+        Returns:
+            Response: JSON formatted data and HTTP status
+        """
+        pass
