6bffbfbccdb22e2efb524b0c184cd82f96d02723
Christopher-Scott <cws837@gmail.com>
2021-03-03 14:04:13 -0500

    FIS-61 Added List Stubs for Portfolio and Debt

--- fiscal/planning_tool/views.py
+++ fiscal/planning_tool/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+class PortfolioList(APIView):
+    """
+    List all accounts, or add a new one
+    """
+
+    def get(self, request):
+        """
+        Get the list of accounts
+
+        Args:
+            request (HttpRequest): The request object from an HTTP request
+
+        Returns:
+            Response: list of account in JSON format
+        """
+        pass
+
+    def post(self, request):
+        """
+        Add a new account
+
+        Args:
+            request (HttpRequest): The request object from an HTTP request
+
+        Returns:
+            Response: JSON formatted data and HTTP status
+        """
+        pass
