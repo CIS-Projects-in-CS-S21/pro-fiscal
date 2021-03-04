cdfd192ea4ab320d3f7589254b54d5bf47232851
Christopher-Scott <cws837@gmail.com>
2021-03-04 13:48:15 -0500

    FIS-61, FIS-62 Added model classes and documentation

--- fiscal/budget_tool/models.py
+++ fiscal/budget_tool/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Expense(models.Model):
+    """
+    A class used to represent the database fields for an expense
+
+    Attributes:
+        expense_id (int): The primary key
+        user_id (int): Foreign key to table 'User'
+        amount (decimal): Expense amount
+        transaction_date (Date): Date of the expense
+        description (String): Short description of the expense
+        category (String): Catefory of the expense
+    """
+
+    pass
