1f019c363e2ded1ac5028d4cfffa056032926d92
Harith Siddiqui <tug84792@temple.edu>
2021-03-04 15:12:57 -0500

    Updated input for home page functions

--- fiscal/assets/home/glossary.js
+++ fiscal/assets/home/glossary.js
@@ -1,7 +1,7 @@
 /**
  * Function that displays a list of finance terms for the user to look up.
  * @param {Array} - takes in an array of JSON Objects that are essentially glossary terms.
- * @returns {Array} - Rendered component an Array containing the list of glossary terms and their definitions.
+ * @returns {Element} - Rendered component an Array containing the list of glossary terms and their definitions.
  * @throws {InvalidArgumentException} If expense is not an array, contains no items, null, etc.
  */
 function read_glossary_from_json_file (glossary) {
--- fiscal/assets/home/how_to_use.js
+++ fiscal/assets/home/how_to_use.js
@@ -1,7 +1,7 @@
 /**
  * Function that displays a list of finance terms for the user to look up.
  * @param {Array} - takes in an array of JSON Objects that are essentially FAQs.
- * @returns {Array} - Rendered component an Array containing the list of FAQs and their answers.
+ * @returns {Element} - Rendered component an Array containing the list of FAQs and their answers.
  * @throws {InvalidArgumentException} - If expense is not an array, contains no items, null, etc.
  */
 function render_guide (faqs) {
