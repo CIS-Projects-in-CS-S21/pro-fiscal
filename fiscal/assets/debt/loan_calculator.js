9b03802adbf34c616be9cc6a8fa662b4f7439124
Harith Siddiqui <tug84792@temple.edu>
2021-03-04 14:45:50 -0500

    Initial JSDoc for Debt/loan_calculator

---
+++ fiscal/assets/debt/loan_calculator.js
@@ -0,0 +1,10 @@
+/**
+ * Function that calculates how long it will take for a loan to be paid off on a monthly basis.
+ * @param {Object} - Debt Item consisting of a debt id, principal, interest rate, amount of payment periods, and expected payment.
+ * @returns {Object} - Returns an approximated amount of time needed to pay off the user's loan.
+ * @throws Will throw an error if null or an empty debt object is inputted.
+ * @throws Will throw an error if a debt_id is not provided, NaN, null, etc.
+ */
+function calculate_expected_payment (expense) {
+
+}
