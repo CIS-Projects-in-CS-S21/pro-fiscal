95127697dcbbac942b7d066e09a362a814d89c03
Harith Siddiqui <tug84792@temple.edu>
2021-03-04 12:42:31 -0500

    Finished how_to_use.js JSDoc

---
+++ fiscal/assets/home/how_to_use.js
@@ -0,0 +1,9 @@
+/**
+ * Function that displays a list of finance terms for the user to look up.
+ * @param {Array} - takes in an array of JSON Objects that are essentially FAQs.
+ * @returns {Array} - Rendered component an Array containing the list of FAQs and their answers.
+ * @throws {InvalidArgumentException} - If expense is not an array, contains no items, null, etc.
+ */
+function how_to_use (faqs) {
+
+}
