73ecae2e0b2cd8b4e870e311112327ec153fc5b1
Christopher-Scott <cws837@gmail.com>
2021-03-04 14:32:37 -0500

    FIS-61, FIS-62 Added serializer classes

---
+++ fiscal/debt_tool/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from debt_tool.models import Debt
+
+class DebtSerializer(serializers.ModelSerializer):
+    """
+    A class to serialize and deserialize Debt instances
+    """
+
+    class Meta:
+        """
+        Attributes:
+            model (__class__): The Debt model class
+            fields (list): A list of the fields in the Debt model
+        """
+        pass
