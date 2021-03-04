36b1819dd93a98d875f272e16a6a2f6ed9b1141b
Harith Siddiqui <tug84792@temple.edu>
2021-03-04 12:38:37 -0500

    Finished Glossary terms JSDoc

---
+++ fiscal/assets/home/glossary.js
@@ -0,0 +1,9 @@
+/**
+ * Function that displays a list of finance terms for the user to look up.
+ * @param {Array} - takes in an array of JSON Objects that are essentially glossary terms.
+ * @returns {Array} - Rendered component an Array containing the list of glossary terms and their definitions.
+ * @throws {InvalidArgumentException} If expense is not an array, contains no items, null, etc.
+ */
+function display_glossary (glossary) {
+
+}
