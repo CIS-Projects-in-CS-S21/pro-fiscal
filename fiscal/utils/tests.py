a1c949fd5f1c4a7bb9a9a6b55eee03ed3617e619
Christopher-Scott <cws837@gmail.com>
2021-03-06 14:32:31 -0500

    FIS-33 Added integration tests for login and logout

--- fiscal/utils/tests.py
+++ fiscal/utils/tests.py
@@ -1,6 +1,11 @@
 from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
 class AccountManagementTest(TestCase):
+
+    """
+    A class for testing the integration of django-rest-auth API endpoints
+    """
 
     def setup(self):
         self.client = Client()
@@ -9,24 +14,55 @@ class AccountManagementTest(TestCase):
         resp = self.client.get('/rest-auth/registration/', format='json')
         self.assertEqual(resp.status_code, 405, "GET request not valid for registration")
 
+
     def test_Register_Failed_Attempt(self):
-        data = {
-                "username": "",
-                "email": "",
-                "password1": "",
-                "password2": ""
-                }
-        resp = self.client.post('/rest-auth/registration/', data)
+        fail_data = {
+            "username": "",
+            "email": "",
+            "password1": "",
+            "password2": ""
+        }
+        resp = self.client.post('/rest-auth/registration/', fail_data)
         self.assertEqual(resp.status_code, 400, "Improper data for registration")
 
     def test_Register_Success(self):
-        data = {
-                "username": "bob",
-                "email": "bob@example.com",
-                "password1": "0C9uasR^K#tt^SxS",
-                "password2": "0C9uasR^K#tt^SxS"
-                }
-        resp = self.client.post('/rest-auth/registration/', data)
+        success_data = {
+            "username": "bob",
+            "email": "bob@example.com",
+            "password1": "bobspassword",
+            "password2": "bobspassword"
+        }
+        resp = self.client.post('/rest-auth/registration/', success_data)
         self.assertEqual(resp.status_code, 201, "Proper data for registration")
 
+    def test_Login_Invalid_Request(self):
+        resp = self.client.get('/rest-auth/login/', format='json')
+        self.assertEqual(resp.status_code, 405, "GET request not valid for login")
 
+    def test_Login_Failed_Attempt(self):
+        fail_data = {
+            "username": "",
+            "email": "",
+            "password": ""
+        }
+        resp = self.client.post('/rest-auth/login/', fail_data)
+        self.assertEqual(resp.status_code, 400, "Improper data for login")
+
+    def test_Login_Success(self):
+        User.objects.create_user(username="logintest", password="logintest")
+        success_data = {
+            "username": "logintest",
+            "password": "logintest"
+        }
+        resp = self.client.post('/rest-auth/login/', success_data)
+        self.assertEqual(resp.status_code, 200, "Proper data for login")
+
+    def test_Logout_Invalid_Request(self):
+        resp = self.client.get('/rest-auth/logout/', format='json')
+        self.assertEqual(resp.status_code, 405, "GET request not valid for logout")
+
+
+    def test_Logout(self):
+        data = {}
+        resp = self.client.post('/rest-auth/logout/', data)
+        self.assertEqual(resp.status_code, 200, "Should always logout")
