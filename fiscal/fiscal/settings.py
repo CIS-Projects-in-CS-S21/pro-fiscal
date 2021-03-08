58023707ad592e5f9fdc7dd170a1cc7642f2ab26
Benjamin Levin <blevin@temple.edu>
2021-03-08 14:05:18 -0500

    removed heroku additions

--- fiscal/fiscal/settings.py
+++ fiscal/fiscal/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,4 +145,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-django_on_heroku.settings(locals())
