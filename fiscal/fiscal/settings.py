b4e882c0af95532f6634e5d76b66fb5af0b3402a
Benjamin Levin <blevin@temple.edu>
2021-03-09 09:27:40 -0500

    updated settings

--- fiscal/fiscal/settings.py
+++ fiscal/fiscal/settings.py
@@ -141,8 +141,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'out'  #. os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
