8faca02bbe9d1eeadb45b97219339e60dff74146
Christopher-Scott <cws837@gmail.com>
2021-03-09 10:50:23 -0500

    FIS-79 Created serializers for portfolio and holding

--- fiscal/planning_tool/serializers.py
+++ fiscal/planning_tool/serializers.py
@@ -1,23 +1,44 @@
 from rest_framework import serializers
-# from planning_tool.models import Portfolio, Holding
+from planning_tool.models import Portfolio, Holding, Security_Type, Account_Type
 
-class PortfolioSerializer(serializers.ModelSerializer):
+class Security_TypeSerializer(serializers.ModelSerializer):
     """
-    A class to serialize and deserialize Portfolio instances
+    A class to serialize and deserialize Security_Type instances to and from JSON data
     """
 
     class Meta:
         """
         Attributes:
-            model (__class__): The Portfolio model class
-            fields (list): A list of the fields in the Portfolio model
+            model (__class__): The Holding model class
+            fields (list): A list of the fields in the Security_Type model
         """
-        pass
+        model = Security_Type
+        fields = ['id', 'type']
+
+
+class Account_TypeSerializer(serializers.ModelSerializer):
+    """
+    A class to serialize and deserialize Account_Type instances to and from JSON data
+    """
+
+    class Meta:
+        """
+        Attributes:
+            model (__class__): The Holding model class
+            fields (list): A list of the fields in the Account_Type model
+        """
+        model = Account_Type
+        fields = ['id', 'type']
+
 
 class HoldingSerializer(serializers.ModelSerializer):
     """
-    A class to serialize and deserialize Holding instances
+    A class to serialize and deserialize Holding instances to and from JSON data
+
+    Attributes:
+        type (Serializer): The serializer for the related security type
     """
+    type = Security_Type(many=False)
 
     class Meta:
         """
@@ -25,4 +46,26 @@ class HoldingSerializer(serializers.ModelSerializer):
             model (__class__): The Holding model class
             fields (list): A list of the fields in the Holding model
         """
-        pass
+        model = Holding
+        fields = ['id', 'portfolio', 'type', 'ticker', 'price', 'shares', 'purchase_date', 'cost_basis']
+
+class PortfolioSerializer(serializers.ModelSerializer):
+    """
+    A class to serialize and deserialize Portfolio instances to and from JSON data
+
+    Attributes:
+        holdings (Serializer): The serializer for related holdings
+        type (Serializer): The serializer for the related security type
+    """
+    holdings = HoldingSerializer(many=True)
+    type = Account_Type(many=False)
+
+    class Meta:
+        """
+        Attributes:
+            model (__class__): The Portfolio model class
+            fields (list): A list of the fields in the Portfolio model
+        """
+        model = Portfolio
+        fields = ['id', 'user', 'type', 'name', 'description', 'balance', 'holdings']
+
