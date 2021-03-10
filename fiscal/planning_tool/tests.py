d655d28d7f8e797dca7465df12ea2b4b714fa76f
Christopher-Scott <cws837@gmail.com>
2021-03-10 12:58:25 -0500

    FIS-79 Updated tests for serializers

--- fiscal/planning_tool/tests.py
+++ fiscal/planning_tool/tests.py
@@ -2,8 +2,7 @@ from django.test import TestCase
 from planning_tool.models import *
 from planning_tool.serializers import *
 
-
-class PortfolioSerializerTest(TestCase):
+class SerializerTest(TestCase):
 
     def setUp(self):
         self.user = User(username="Test", password="fiscaltest")
@@ -14,21 +13,25 @@ class PortfolioSerializerTest(TestCase):
         self.sec_type.save()
 
         self.acct_attr = {
+            'id': None,
             'user': self.user,
             'account_type': self.acct_type,
             'name': "MyTestAccount",
             'description': "Just a Test",
-            'balance': 100.00
+            'balance': 100.00,
         }
 
         self.acct = Portfolio.objects.create(**self.acct_attr)
 
         self.holding_attr = {
+            'id': None,
             'portfolio': self.acct,
             'security_type': self.sec_type,
             'ticker': "GOOG",
             'price': 1000.00,
-            'shares': 20
+            'shares': 20,
+            'cost_basis': None,
+            'purchase_date': None
         }
 
         self.holding = Holding.objects.create(**self.holding_attr)
@@ -36,8 +39,12 @@ class PortfolioSerializerTest(TestCase):
         self.port_serializer = PortfolioSerializer(instance=self.acct)
         self.holding_serializer = HoldingSerializer(instance=self.holding)
 
-    def test_contains_expected_fields(self):
+    def test_portfolio_contains_expected_fields(self):
         data = self.port_serializer.data
 
-        self.assertEqual((set(data.keys())), set(self.acct_attr.keys()).union(['id', 'holdings']))
+        self.assertEqual((set(data.keys())), set(self.acct_attr.keys()).union(['holdings']))
 
+    def test_holding_contains_expected_fields(self):
+        data = self.holding_serializer.data
+
+        self.assertEqual((set(data.keys())), set(self.holding_attr.keys()))
