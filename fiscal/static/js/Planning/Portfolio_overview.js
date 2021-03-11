c55fe0e45b76a92e149df109b2ff0181469c6ceb
Jsndng408 <tuh22928@temple.edu>
2021-03-10 20:13:30 -0500

    Prepared JSON file directory for testing, and general testing of code for asynchronous JavaScript

--- fiscal/static/js/Planning/Portfolio_overview.js
+++ fiscal/static/js/Planning/Portfolio_overview.js
@@ -4,7 +4,7 @@
  * @throws {InvalidArgumentException} when the user enter no value id.
  * @returns {Array} arrays of portfolios
  */
-function get_portfolios (user_id) {
+function get_portfolios(user_id) {
 
 }
 
@@ -15,7 +15,7 @@ function get_portfolios (user_id) {
  * @returns {boolean} confirmation status
  * @throws {InvalidArgumentException} if the user enters invalid id or account name.
  */
-function create_portfolio (user_id, account_name) {
+function create_portfolio(user_id, account_name) {
 
 }
 
@@ -26,7 +26,7 @@ function create_portfolio (user_id, account_name) {
  * @returns {Object} return user account information to make changes and update.
  * @throws {InvalidArgumentException} if the user enters no value id or account name.
  */
-function update_portfolio (portfolio_id, account_name) {
+function update_portfolio(portfolio_id, account_name) {
 
 }
 
@@ -36,8 +36,7 @@ function update_portfolio (portfolio_id, account_name) {
  * @returns {boolean} Returns confirmation status.
  * @throws {InvalidArgumentException} if the user enters no value id.
  */
-
-function delete_portfolio (portfolio_id) {
+function delete_portfolio(portfolio_id) {
 
 }
 
@@ -48,7 +47,7 @@ function delete_portfolio (portfolio_id) {
  * @returns {Object} the updated portfolio account to the user.
  * @throws {InvalidArgumentException} if the user add no value or -ve value.
  */
-function add_holding (portfolio_id, amount) {
+function add_holding(portfolio_id, amount) {
 
 }
 
@@ -58,47 +57,91 @@ function add_holding (portfolio_id, amount) {
  * @return {float} return holding balance to the user to make an update.
  * @throws {InvalidArgumentException} if the user enters wrong portfolio id.
  */
-function update_holding (portfolio_id) {
+function update_holding(portfolio_id) {
 
 }
+
 /**
  * Function that deletes the holding amount.
  * @param {int} portfolio_id the user identification number.
  * @returns {boolean} Returns confirmation status.
  * @throws {InvalidArgumentException} if portfolio is invalid.
  */
-function delete_holding (portfolio_id) {
+function delete_holding(portfolio_id) {
+
 }
+
 /**
  * Function that retrieval of user's answers data.
  * @param {int} user_id the user identification number.
  * @returns {Array} Return arrays of user answers.
  * @throws {InvalidArgumentException} if user id is invalid.
  */
-function fetch_saved_answers (user_id) {
+function fetch_saved_answers(user_id) {
+
 }
+
 /**
  * Function that submit a user's questionnaire answers.
  * @param {int} user_id the user identification number.
  * @throws {InvalidArgumentException} if user id is invalid.
  */
-function submit_answer (user_id) {
+function submit_answer(user_id) {
+
 }
+
 /**
  * Function that updates the user's questionnaire answers.
  * @param {int} user_id the user identification number.
  * @returns {String} return answers to make changes and update.
  * @throws {InvalidArgumentException} if the user enters no value id.
  */
-function update_answers (user_id) {
+function update_answers(user_id) {
 
 }
+
 /**
  * Function that clear the user's questionnaire answers.
  * @param {int} user_id the user identification number.
  * @throws {InvalidArgumentException} if the user enters no value id.
  */
-function clear_answers (user_id) {
+function clear_answers(user_id) {
+
 }
 
+/**
+ * 
+ */
+function render_portfolio_overview() {
 
+    const getPortfolioData = () => {
+        let portfolios = "";
+
+        fetch("/static/json/portfolio_test.json")
+            .then(response => {
+                return response.json();
+            })
+            .then((data) => {
+                portfolios = data;
+            })
+            .then(() => {
+                // Do all the interface stuff here
+                handleUserPortfolios(portfolios);
+            })
+    };
+
+    const handleUserPortfolios = (portfolios) => {
+        console.log(portfolios);
+    }
+
+    getPortfolioData();
+
+    let content = `
+        <h2>Portfolio!</h2>
+    `;
+
+    let elem = document.createElement("div");
+    elem.innerHTML = content;
+
+    return elem;
+}
