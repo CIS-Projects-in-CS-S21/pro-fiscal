15c9f8490f11fab32933a4f06f21a664a8e8ddc3
Ubuntu <ubuntu@ip-172-31-36-39.us-east-2.compute.internal>
2021-03-13 13:07:44 +0000

    verified aws dev server deployment

---
+++ fiscal/staticfiles/js/Planning/monte_carlo.js
@@ -0,0 +1,18 @@
+/**
+ * Function stores the collection of forecast data and provide the visualization of the range of future values output.
+ * @param {Array} forecast_data The data that will be used to render potential future value rates for the output.
+ * @throws {InvalidArgumentException} if the value is null.
+ * @returns {Array} Return arrays of future value.
+ */
+function render_future_value (forecast_data) {
+
+}
+
+/**
+ * Function that give a collection of future value of data.
+ * @param {Array} forecast_data The data that will be used to render potential future value rates for the output.
+ * @returns {Array} Return a collection of forecast amount of data.
+ * @throws {InvalidArgumentException} if the value of data is null.
+ */
+function future_value_chart (forecast_data) {
+}
