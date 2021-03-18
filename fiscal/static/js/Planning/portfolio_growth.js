28404a9d9bc9e3b92ad81ed6139a082fcfc07930
Jsndng408 <tuh22928@temple.edu>
2021-03-18 10:32:54 -0400

    Using Chart.js to render an initial portfolio growth chart

--- fiscal/static/js/Planning/portfolio_growth.js
+++ fiscal/static/js/Planning/portfolio_growth.js
@@ -1,9 +1,11 @@
 function render_portfolio_growth() {
 
+    let dateData = [], balanceData = [];
+
     const getPortfolioChangeData = () => {
         let portfolios = "";
 
-        fetch("/static/json/portfolio_diversification.json")
+        fetch("/static/json/portfolio_growth.json")
             .then(response => {
                 return response.json();
             })
@@ -11,18 +13,57 @@ function render_portfolio_growth() {
                 portfolios = data;
             })
             .then(() => {
-                handleUserPortfolios(portfolios);
+                extractPortfolioBalanceDates(portfolios, 0);
+            })
+            .then(() => {
+                createAreaChart();
             })
     }
 
-    const handleUserPortfolios = (portfolios) => {
-        console.log(portfolios);
+    const extractPortfolioBalanceDates = (portfolios, index) => {
+        let item = portfolios[index];
+
+        dateData = item["dates"];
+        balanceData = item["balances"];
+
+        dateData.push(item["latest_balance"]["date"]);
+        balanceData.push(item["latest_balance"]["balance"]);
+    }
+
+    function createAreaChart() {
+        var ctx = document.getElementById('myChart').getContext('2d');
+
+        console.log(balanceData);
+        console.log(dateData);
+
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+
+            // The data for our dataset
+            data: {
+                labels: dateData,
+                datasets: [{
+                    label: "User's Portfolio Growth",
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: balanceData
+                }]
+            },
+
+            // Configuration options go here
+            options: {}
+        });
     }
 
     getPortfolioChangeData();
 
+    let portfolioDiv = document.createElement("div");
+
     let contents = document.createElement("h3");
     contents.innerText = "Testing Portfolio Growth";
+
+    portfolioDiv.appendChild(contents);
 
     return contents;
 }
