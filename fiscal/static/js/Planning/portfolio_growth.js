778a8b4ece7fbb3fd4928a9bb0bd9ce1001e7cad
Harith Siddiqui <tug84792@temple.edu>
2021-03-22 14:26:44 -0400

    Fixed Stocks bug in code

--- fiscal/static/js/Planning/portfolio_growth.js
+++ fiscal/static/js/Planning/portfolio_growth.js
@@ -11,107 +11,108 @@ function render_portfolio_growth() {
         "rgb(133, 85, 237)",
         "rgb(133, 237, 85)",
     ];
-  const reduceDataset = (dataset) => {
-    const currerntVal = [];
+    const reduceDataset = (dataset) => {
+        const currerntVal = [];
 
-    dataset.forEach((newVal) => {
-      const foundIndex =
-        currerntVal &&
-        currerntVal.findIndex((a) => a.security_type === newVal.security_type);
+        dataset.forEach((newVal) => {
+            const foundIndex =
+                currerntVal &&
+                currerntVal.findIndex((a) => a.security_type === newVal.security_type);
 
-      if (foundIndex > -1) {
-        currerntVal[foundIndex].price += newVal.price;
-        currerntVal[foundIndex].shares += newVal.shares;
-        currerntVal[foundIndex].total =
-          currerntVal[foundIndex].price * currerntVal[foundIndex].shares;
-      } else {
-        currerntVal.push({
-          security_type: newVal.security_type,
-          price: newVal.price,
-          shares: newVal.shares,
-          total: newVal.price * newVal.shares,
+            if (foundIndex > -1) {
+                currerntVal[foundIndex].price += newVal.price;
+                currerntVal[foundIndex].shares += newVal.shares;
+                currerntVal[foundIndex].total += newVal.total ||
+                    newVal.price * newVal.shares;
+            } else {
+                currerntVal.push({
+                    security_type: newVal.security_type,
+                    price: newVal.price,
+                    shares: newVal.shares,
+                    total: newVal.price * newVal.shares,
+                });
+            }
         });
-      }
-    });
 
-    return currerntVal;
-  };
+        return currerntVal;
+    };
 
-  const createDataset = (dataset) => {
-    const combinedDataset = [];
+    const createDataset = (dataset) => {
+        const combinedDataset = [];
 
-    dataset.forEach((account) => {
-      combinedDataset.push(...reduceDataset(account.holdings));
-    });
+        dataset.forEach((account) => {
+            combinedDataset.push(...reduceDataset(account.holdings));
+        });
 
-    const finalDataset = reduceDataset(combinedDataset);
+        const finalDataset = reduceDataset(combinedDataset);
 
-    const labels = finalDataset.reduce((labels, currentVal) => {
-      if (!labels) {
-        labels = [];
-      }
-      labels.push(currentVal.security_type);
+        const labels = finalDataset.reduce((labels, currentVal) => {
+            if (!labels) {
+                labels = [];
+            }
+            labels.push(currentVal.security_type);
 
-      return labels;
-    }, []);
-    const values = finalDataset.reduce((labels, currentVal) => {
-      if (!labels) {
-        labels = [];
-      }
-      labels.push(currentVal.total);
+            return labels;
+        }, []);
+        const values = finalDataset.reduce((labels, currentVal) => {
+            if (!labels) {
+                labels = [];
+            }
+            labels.push(currentVal.total);
 
-      return labels;
-    }, []);
+            return labels;
+        }, []);
 
-    return { labels, values, finalDataset };
-  };
+        return {labels, values, finalDataset};
+    };
 
-  function createPieChart() {
-    var ctx = document.getElementById("myChart").getContext("2d");
+    function createPieChart() {
+        var ctx = document.getElementById("myChart").getContext("2d");
 
-    return fetch("/static/json/portfolio_test.json")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        const dataSet = createDataset(data.portfolio_accounts);
+        return fetch("/static/json/portfolio_test.json")
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                const dataSet = createDataset(data.portfolio_accounts);
 
-        var chart = new Chart(ctx, {
-          // The type of chart we want to create
+                var chart = new Chart(ctx, {
+                    // The type of chart we want to create
 
-          // The data for our dataset
+                    // The data for our dataset
 
-          type: "doughnut",
-          data: {
-            labels: dataSet.labels,
-            datasets: [{ data: dataSet.values, backgroundColor: COLOR_OPTIONS }],
+                    type: "doughnut",
+                    data: {
+                        labels: dataSet.labels,
+                        datasets: [{data: dataSet.values, backgroundColor: COLOR_OPTIONS}],
 
-          },
+                    },
                     options: {
-        animate: true,
-tooltips: {
-      callbacks: {
-          label:  (val, data) =>
-             (`$${data.datasets[val.datasetIndex].data[val.index].toLocaleString()}`)
+                        animate: true,
+                        tooltips: {
+                            callbacks: {
+                                label: (val, data) =>
+                                    (`$${data.datasets[val.datasetIndex].data[val.index].toLocaleString()}`)
 
-        }
-        }}})
+                            }
+                        }
+                    }
+                })
 
-        // Used to remove the Chart when we exit out of the Portfolio Growth Page
-        function removeChart() {
-          chart.destroy();
-        }
+                // Used to remove the Chart when we exit out of the Portfolio Growth Page
+                function removeChart() {
+                    chart.destroy();
+                }
 
-        window.addEventListener("hashchange", removeChart);
-      });
-  }
+                window.addEventListener("hashchange", removeChart);
+            });
+    }
 
-  let contents = document.createElement("h3");
-  contents.innerText = "Your Portfolios - Diversification"; // Username here
+    let contents = document.createElement("h3");
+    contents.innerText = "Your Portfolios - Diversification"; // Username here
 
-  createPieChart();
+    createPieChart();
 
-  return contents;
+    return contents;
 }
-
