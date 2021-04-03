b62d8d106c9cbf068903184635e45acc7e13cb2f
Benjamin Levin <blevin@temple.edu>
2021-04-03 14:16:06 -0400

    added random number generation

--- machine_learning/gen_data.py
+++ machine_learning/gen_data.py
@@ -20,6 +20,7 @@ cat_names = ['Housing', 'Transportation', 'Debt', 'Insurance',
                 'Non-Essentials', 'Other']
 
 def pick_cat():
+    #returns a random number of a category type
     return (random.randint(0, len(cat_names)-1))
 
 def gen_datetime(min_year, max_year):
@@ -30,6 +31,14 @@ def gen_datetime(min_year, max_year):
     out = start + (end - start) * random.random()
     return (out.date())
 
+def get_price(category):
+    #returns a random price in a uniform range rounded to 2 decimal places
+    cat_prices = [(500.0, 5000.0), (50.0, 600.0), (100.0, 10000.0), (50.0, 1500.0),
+                  (60.0, 2000.0), (100.0, 20000.0), (50.0, 20000.0), (50.0, 20000.0),
+                  (0.0, 12000.0), (50.0, 500.0), (0.0, 1000.0), (30.0, 1000.0),
+                  (0.0, 1000.0), (0.0, 1000.0)]
+    return round(random.uniform(cat_prices[category][0], cat_prices[category][1]), 2)
+
 def main():
     random.seed(123333)
     num_entries = 25
@@ -39,7 +48,7 @@ def main():
         for i in range(num_entries):
             cat_num = pick_cat()
             # date, detail, cost, category
-            data_writer.writerow([gen_datetime(1970, 2020)]+[cat_names[cat_num]])
+            data_writer.writerow([gen_datetime(1970, 2020)]+[get_price(cat_num)]+[cat_names[cat_num]])
         
 
 if __name__ == "__main__":
