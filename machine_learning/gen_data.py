ce1d8eafae1f5cecca79c042fb5b07ff6e54b07d
Benjamin Levin <blevin@temple.edu>
2021-04-03 15:11:23 -0400

    added descriptions and sample output

--- machine_learning/gen_data.py
+++ machine_learning/gen_data.py
@@ -19,6 +19,19 @@ cat_names = ['Housing', 'Transportation', 'Debt', 'Insurance',
                 'Education', 'Groceries/Household', 'Entertainment', 'Essentials',
                 'Non-Essentials', 'Other']
 
+cat_desciptions = [['Rent payment', 'Morgage payment', 'property taxes', 'HOA dues',
+        'Home repairs'], ['Bus fair', 'Subway ticket', 'Train ticket', 'Plane ticket',
+        'Gasoline', 'Toll', 'Uber fair'], ['Student Loans', 'Car Payment', 'Phone Payment',
+        'Credit Card payment', 'loan payment'], ['Car Insurance', 'Health Insurance',
+        'Flood insurance', 'Life insurance', 'Fire Insurance'], ['Gas bill', 'Internet bill', 
+        'electricity', 'Water', 'Oil payment', 'Phone bill', 'Cable bill'], ['Rainy day fund', 
+        'savings account', 'college fund'], ['PCP visit', 'Medication refill', 'Eye exam', 
+        'Dentist visit'], ['401k', 'IRA'], ['Textbooks', 'Tuition', 'Online course', 'Exam fees',
+        'School supplies', 'Pet food'], ['Grocery shopping', 'Cleaning supplies', 'Homegoods'],
+        ['Movies', 'Books', 'Museums', 'Vacation', 'Sports game', 'Dining out'], ['Emergency',
+        'Vet bill', 'Sudden travel expense'], ['New TV', 'Black friday sale', 'Ski tickets'],
+        ['GME YOLO']]
+
 def pick_cat():
     #returns a random number of a category type
     return (random.randint(0, len(cat_names)-1))
@@ -39,16 +52,22 @@ def get_price(category):
                   (0.0, 1000.0), (0.0, 1000.0)]
     return round(random.uniform(cat_prices[category][0], cat_prices[category][1]), 2)
 
+def get_description(category):
+    #returns a random description for the category
+    des_num = random.randint(0, len(cat_desciptions[category])-1)
+    return cat_desciptions[category][des_num]
+
 def main():
     random.seed(123333)
-    num_entries = 25
+    num_entries = 250
     file_name = "data_test1.csv"
     with open(file_name, 'w', newline='') as csvfile:
         data_writer = csv.writer(csvfile)
         for i in range(num_entries):
             cat_num = pick_cat()
             # date, detail, cost, category
-            data_writer.writerow([gen_datetime(1970, 2020)]+[get_price(cat_num)]+[cat_names[cat_num]])
+            data_writer.writerow([gen_datetime(1970, 2020)]
+                + [get_price(cat_num)] + [get_description(cat_num)] + [cat_names[cat_num]])
         
 
 if __name__ == "__main__":
