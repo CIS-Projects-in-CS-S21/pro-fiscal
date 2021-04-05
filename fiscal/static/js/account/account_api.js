502c74b21ff89ae4bc087a83235c53f535710f84
Jsndng408 <tuh22928@temple.edu>
2021-04-05 10:44:33 -0400

    Username update API and form rendered

--- fiscal/static/js/account/account_api.js
+++ fiscal/static/js/account/account_api.js
@@ -38,6 +38,43 @@ account_api.getUserInfo = function (data, successHandler, errorDOM) {
 }
 
 /**
+ * Function that updates the user's username.
+ * @param {Object} data Username to be sent to request user information.
+ * @param {function} successHandler Callback function to handle the data.
+ * @param {Node} errorDOM Element where potential error messages will be added to.
+ * @returns {boolean} Confirmation status of the operation.
+ */
+ account_api.updateUsername = function (data, successHandler, errorDOM) {
+    let status = false;
+    let init = {
+        method: 'PATCH', // Not PUT in this case
+        headers: {
+            'Content-Type': 'application/json',
+            "Accept": "application/json",
+            'Authorization': "token " + localStorage.getItem("key")
+        },
+        body: JSON.stringify(data)
+    }
+
+    fetch("/rest-auth/user/", init)
+        .then((response) => {
+            if (!response.ok) {
+                response.text().then(text => { throw Error(text) });
+            }
+            status = true;
+            return response.json();
+        }).then(data => {
+            successHandler(data);
+        }).catch(error => {
+            status = false;
+            console.error(error);
+            errorDOM.innerText = error;
+        })
+
+    return status;
+}
+
+/**
  * Function that allows the user to change their password.
  * @param {Object} data Object containing the new password and a confirmation password to update the user's password.
  * @param {function} successHandler Callback function to handle the data.
--- fiscal/static/js/account/profile.js
+++ fiscal/static/js/account/profile.js
@@ -94,6 +94,72 @@ function userProfile() {
         return form;
     }
 
+    function renderUpdateUsernameForm(saveFunc, cancelFunc) {
+        let form = {};
+
+        form.container = document.createElement("div");
+
+        let title = document.createElement("h2");
+        title.innerText = "Update your Username";
+        title.classList.add("text-center");
+
+        let label_old_username = document.createElement("label");
+        label_old_username.innerText = "Your previous Username";
+        form.old_username = document.createElement('input');
+        form.old_username.type = 'text';
+        form.old_username.classList.add("form-control");
+        form.old_username.value = localStorage.getItem("username");
+        form.old_username.disabled = true;
+
+        let new_username = document.createElement("label");
+        new_username.innerText = "New Username";
+        form.new_username = document.createElement('input');
+        form.new_username.type = 'text';
+        form.new_username.classList.add("form-control");
+
+        let buttonDiv = document.createElement("div");
+        buttonDiv.classList.add("d-flex");
+
+        let buttonGroupSubmit = document.createElement("div");
+        buttonGroupSubmit.classList.add("btn-group", "mr-auto");
+
+        let buttonGroupCancel = document.createElement("div");
+        buttonGroupCancel.classList.add("btn-group", "ml-auto");
+
+        form.submit = createButton({
+            type: "btn-success",
+            text: "Submit",
+            onclickhandler: saveFunc
+        });
+
+        form.submit.classList.add("mr-2");
+
+        let cancel = createButton({
+            type: "btn-danger",
+            text: "Cancel",
+            onclickhandler: cancelFunc
+        });
+
+        cancel.classList.add("mr-2");
+
+        form.container.appendChild(title);
+        form.container.appendChild(label_old_username);
+        form.container.appendChild(form.old_username);
+        form.container.appendChild(new_username);
+        form.container.appendChild(form.new_username);
+        form.container.appendChild(document.createElement("br"));
+
+        buttonGroupSubmit.appendChild(form.submit);
+        buttonGroupCancel.appendChild(cancel);
+
+        buttonDiv.appendChild(buttonGroupSubmit);
+        buttonDiv.appendChild(buttonGroupCancel);
+
+        form.container.appendChild(buttonDiv);
+
+        return form;
+    }
+
     let profileDiv = document.createElement("div");
 
     let username = localStorage.getItem("username");
@@ -107,29 +173,60 @@ function userProfile() {
     let accountDateHolder = document.createElement("p");
     accountDateHolder.innerText = "Created Account on (Date)";
 
-    let updateAccountButton = createButton({
+    let buttonDiv = document.createElement("div");
+    buttonDiv.classList.add("d-flex");
+
+    let buttonGroupUsername = document.createElement("div");
+    let buttonGroupPassword = document.createElement("div");
+    
+    let updateUsernameButton = createButton({
+        type: "btn-secondary",
+        text: "Update your Username"
+    });
+
+    let updatePasswordButton = createButton({
         type: "btn-info",
         text: "Update your Password"
     });
 
-    updateAccountButton.addEventListener("click", function () {
+    updateUsernameButton.addEventListener("click", function () {
+        let form = renderUpdateUsernameForm(function () {
+            modal.hideModal();
+            console.log("Hello World");
+        }, function () {
+            modal.hideModal();
+        });
+
+        modal.renderForm(form.container);
+    });
+
+    updatePasswordButton.addEventListener("click", function () {
         let form = renderUpdatePasswordForm(function () {
             modal.hideModal();
             console.log("Hello World");
         }, function () {
             modal.hideModal();
-        })
+        });
 
         modal.renderForm(form.container);
     });
 
+    buttonGroupUsername.classList.add("btn-group", "ml-auto", "mr-auto");
+    buttonGroupPassword.classList.add("btn-group", "ml-auto", "mr-auto");
+
     dashboard.appendChild(header);
     dashboard.appendChild(accountDateHolder);
 
-
     profileDiv.appendChild(modal);
     profileDiv.appendChild(dashboard);
-    profileDiv.appendChild(updateAccountButton);
+
+    buttonGroupUsername.appendChild(updateUsernameButton);
+    buttonGroupPassword.appendChild(updatePasswordButton);
+
+    buttonDiv.appendChild(buttonGroupUsername);
+    buttonDiv.appendChild(buttonGroupPassword);
+    
+    profileDiv.appendChild(buttonDiv);
 
     return profileDiv;
 }
