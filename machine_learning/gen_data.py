28b6028eed81a65bed3e062c3b6edf8a2dda4142
Benjamin Levin <blevin@temple.edu>
2021-04-05 16:22:03 -0400

    began added more variety to cat-desc

--- machine_learning/gen_data.py
+++ machine_learning/gen_data.py
@@ -4,10 +4,10 @@ from datetime import datetime, timedelta
 
 """
 Categories:
-housing, transportation, debt, insurance, utilities, 
-medical/healthcare, savings, retirement, education,
-groceries/household, entertainment, essentials, non-essentials,
-other
+housing:0, transportation:1, debt:2, insurance:3, utilities:4, 
+medical/healthcare:5, savings:6, retirement:7, education:8,
+groceries/household:9, entertainment:10, essentials:11, non-essentials:12,
+other:13
 
 List of entries for each category
 
@@ -57,17 +57,47 @@ def get_description(category):
     des_num = random.randint(0, len(cat_desciptions[category])-1)
     return cat_desciptions[category][des_num]
 
-def main():
-    random.seed(123333)
-    num_entries = 250
-    file_name = "data_test1.csv"
+def populate_cat_desc(cat_desc, purchase_list, name_list, total_to_add):
+    #adds total_to_add random descriptions to cat_desc consisting of ranomd
+    # combination of entires from purchase_list and name_list
+    for i in range(total_to_add):
+        purchase_pos = random.randint(0, len(purchase_list)-1)
+        name_pos = random.randint(0, len(name_list)-1)
+        cat_desc.append(purchase_list[purchase_pos]+name_list[name_pos])
+
+def add_desc_to_cats():
+    #adds additional descriptions to the categories
+    #starts of grocery store entries to add
+    grocery_starts = ['Food from ', 'Groceries from ', 'Shopping at ']
+    #list of population regional US grocery store names
+    grocery_names = ['Safeway', 'Albertsons', 'Vons', 'Pavilions', 'Jewel-Osco',
+        'Acme Markets', 'Shaw\'s', 'Andronico\'s', 'Carrs', 'Haggen', 'Lucky',
+        'Pavilions', 'Randalls', 'Star Market', 'Tom Thumb', 'United Supermarkets',
+        'Food Lion', 'Stop & Shop', 'Giant Food Stores', 'Hannaford', 'Giant Food',
+        'Harris Teeter', 'King Soopers', 'Roundy\'s', 'Smith\'s Food and Drug',
+        'Fred Meyer', 'QFC', 'Ruler Foods', 'Food 4 Less', 'City Market', 'Target',
+        'Sam\'s Club', 'Bashas\'', 'Raley\'s', 'Save Mart', 'Smart & Final', 
+        'WinCo Foods', 'Fareway', 'Hy-Vee', 'Meijer', 'Schnucks', 'SpartanNash',
+        'Giant Eagle', 'Market Basket', 'Price Chopper', 'Price Rite', 'ShopRite',
+        'Tops', 'Weis', 'Wegmans', 'Brookshire\'s', 'Brookshire Brothers',
+        'Food City', 'Harps Food Stores', 'Ingles Markets', 'Piggle Wiggly',
+        'Publix', 'BI-LO', 'Harvey\'s', 'Winn-Dixie']
+    #populates the grocery category descriptions
+    populate_cat_desc(cat_desciptions[9], grocery_starts, grocery_names, 50)
+
+def gen_data(file_name, num_entries):
+    #Outputs num_entries number of lines to the csv file file_name
     with open(file_name, 'w', newline='') as csvfile:
         data_writer = csv.writer(csvfile)
         for i in range(num_entries):
             cat_num = pick_cat()
             # date, detail, cost, category
-            data_writer.writerow([gen_datetime(1970, 2020)]
+            data_writer.writerow([gen_datetime(2010, 2020)]
                 + [get_price(cat_num)] + [get_description(cat_num)] + [cat_names[cat_num]])
+def main():
+    random.seed(123333)
+    add_desc_to_cats()
+    gen_data("data_test1.csv", 250)
         
 
 if __name__ == "__main__":
