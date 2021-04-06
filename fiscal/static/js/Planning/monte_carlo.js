30f95cf76c09ba620b576dd94cf1ef50d3a48cf0
Harith Siddiqui <tug84792@temple.edu>
2021-04-06 15:40:05 -0400

    finalized code for Monte Carlo histogram

--- fiscal/static/js/Planning/monte_carlo.js
+++ fiscal/static/js/Planning/monte_carlo.js
@@ -1,44 +1,193 @@
 /**
- * Function stores the collection of forecast data and provide the visualization of the range of future values output.
- * @param {Array} forecast_data The data that will be used to render potential future value rates for the output.
- * @throws {InvalidArgumentException} if the value is null.
- * @returns {Array} Return arrays of future value.
+ * Helper function for Charting that calculates the difference between the start date and the days that the portfolio's value was updated.
+ * @param {Array} dateData Array of Dates for a Portfolio
+ * @returns {Array} Number of Days since the start of the Portfolio
  */
-function render_future_value(forecast_data) {
+function calculate_days_after(dateData) {
+    const start_date = new Date(dateData[0]);
+    let daysAfter = [];
 
+    for (let i = 0; i < dateData.length; i++) {
+        let difference = days_after_update(new Date(dateData[i]), start_date);
+        daysAfter.push(difference);
+    }
+
+    return daysAfter;
 }
 
+Array.prototype.createHistogram = function (numOfBins) {
+    const obj = []
+
+    const low = Math.min(...this)
+    const high = Math.max(...this)
+    const range = high - low;
+
+    console.log(low, high, range)
+
+    for (i = 0; i < numOfBins; i++) {
+        const start = i === 0 ? low : low + Math.floor(i * range / numOfBins) + 1
+
+        const end = i === numOfBins ? high : low + Math.ceil((i + 1) * range / numOfBins)
+
+        obj.push({
+            label: `${start} - ${end}`,
+            val: this.filter(a => (a >= start && a <= end)).length,
+        })
+    }
+
+    return obj
+}
+
+
 /**
- * Function that give a collection of future value of data.
- * @param {Array} forecast_data The data that will be used to render potential future value rates for the output.
- * @returns {Array} Return a collection of forecast amount of data.
- * @throws {InvalidArgumentException} if the value of data is null.
+ * Helper function that parses dates in 'YYYY-MM-DD' string format to a date object
+ * @param dateString
+ * @returns {Date}
  */
+function parseDate(dateString) {
+    let arr = dateString.split('-');
+    let year = parseInt(arr[0]);
+    let month = parseInt(arr[1]) - 1;
+    let day = parseInt(arr[2]);
+
+    return new Date(year, month, day);
+}
+
 function future_value_chart() {
-    const createVals = (amnt) => {
-        const vals = []
-        for (i = 0; i < amnt; i++) {
-            vals.push(Math.floor(Math.random() * 100))
-            // vals.push(i)
-        }
-        return vals
+
+    const JSON_OBJ = {
+        "id": 1,
+        "user": 13,
+        "results": {
+            "future_values": [
+                1046.56,
+                1154.94,
+                1542.59,
+                1387.89,
+                1196.59,
+                1170.66,
+                1126.76,
+                1317.22,
+                1304.45,
+                1034.08,
+                1134.68,
+                1463.98,
+                1426.3,
+                1061.88,
+                1063.89,
+                1227.6,
+                914.21,
+                1407.47,
+                1785.84,
+                1372.43,
+                1102.14,
+                1019.35,
+                1453.75,
+                1129.94,
+                1050.23,
+                1022.35,
+                1285.19,
+                989.67,
+                1293.87,
+                1044.2,
+                1145.93,
+                1180.85,
+                1233.69,
+                957.45,
+                1197.92,
+                938.91,
+                922.07,
+                1342.5,
+                1093.55,
+                1131.41,
+                978.18,
+                1264.0,
+                1072.6,
+                992.54,
+                1305.09,
+                1341.14,
+                883.39,
+                1279.41,
+                1041.69,
+                816.95,
+                1281.63,
+                1440.59,
+                1104.21,
+                1223.67,
+                890.39,
+                1106.15,
+                1378.08,
+                1124.14,
+                1174.4,
+                1526.44,
+                1312.38,
+                1081.32,
+                1083.36,
+                1062.83,
+                1659.96,
+                1227.95,
+                1369.67,
+                1016.39,
+                938.22,
+                1578.99,
+                1125.93,
+                1279.15,
+                1343.06,
+                1435.16,
+                1311.89,
+                1306.03,
+                1610.26,
+                945.09,
+                923.05,
+                1209.17,
+                1083.22,
+                1583.71,
+                1194.65,
+                1133.85,
+                1117.75,
+                1432.3,
+                998.02,
+                1143.44,
+                1039.65,
+                1706.64,
+                982.13,
+                1006.56,
+                1316.14,
+                1855.39,
+                1176.8,
+                1652.08,
+                1494.07,
+                1340.12,
+                1326.41,
+                1055.55,
+                1286.75
+            ]
+        },
+        "date": "2021-04-03T14:40:46.314997Z"
     }
 
     Array.prototype.createHistogram = function (numOfBins) {
+
+
         const obj = []
 
         const low = Math.min(...this)
         const high = Math.max(...this)
         const range = high - low;
+        let lastStart = null;
 
         for (i = 0; i < numOfBins; i++) {
-            const start = i === 0 ? low : low + Math.floor(i * range / numOfBins) + 1
+            const start = lastStart || Math.floor((i === 0 ? low : low + Math.floor(i * range / numOfBins) + 1) / 100) * 100
 
-            const end = i === numOfBins ? high : low + Math.ceil((i + 1) * range / numOfBins)
+
+            const end = Math.ceil((i === numOfBins ? high : low + Math.ceil((i + 1) * range / numOfBins)) / 100) * 100
+
+            lastStart = end;
+
 
             obj.push({
-                label: `${start} - ${end}`,
-                val: this.filter(a => (a >= start && a <= end)).length,
+                label: `${start}`,
+                val: this.filter(a => (a > start && a < end)).length,
             })
         }
 
@@ -49,45 +198,36 @@ function future_value_chart() {
         return this.sort((a, b) => a.val % 2 - b.val % 2 || (a.val % 2 ? b.val - a.val : a.val - b.val))
     }
 
-    const vals = createVals(100);
-    const histo = vals.createHistogram(5).bellCurve();
+    const histo = JSON_OBJ.results.future_values.createHistogram(10)
+
 
     var labels = histo.map(function (e) {
         return e.label;
     });
 
-    var dataItems = histo.map(function (e) {
+    var data = histo.map(function (e) {
         return e.val;
-    });;
+    });
 
-
-    function createBarChart(dataItems) {
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: labels,
-                datasets: [{
-                    fill: false,
-                    data: dataItems,
-                }]
-            },
-            options: {
-                responsive: true
-            }
-        });
-
-        // Used to remove the Chart when we exit out of the Portfolio Growth Page
-        function removeChart() {
-            myChart.destroy();
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        responsive: true,
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Jason',
+                fill: false,
+                data: data,
+            }]
         }
+    })
+    /* range = Math.max(array) - Math.min(array);
+    bucket_range = range/numBuckets;
+    for(i in array.length){
+      index = Math.floor(array[i]  / bucket_range);
+      new_array[index]++;
+    } */
 
-        window.addEventListener('hashchange', removeChart);
-    }
 
-    createBarChart(dataItems);
-
-    let title = document.createElement("h3");
-    title.innerText = "Monte Carlo Future Value Range Simulation";
-    return title;
 }
