16b865522e1196f9a4a3114dddd19d5f95aa1839
Christopher-Scott <cws837@gmail.com>
2021-04-08 11:59:40 -0400

    FIS-105 Combined chart and simulation controls

--- fiscal/static/js/Planning/monte_carlo.js
+++ fiscal/static/js/Planning/monte_carlo.js
@@ -54,6 +54,7 @@ function render_monte_interface(){
             // Call API
             if(valid) {
                 let status_code;
+                message.innerText = "";
                 monte_api.start_sim(input_data)
                     .then((response) => {
                         status_code = response.status;
@@ -75,6 +76,7 @@ function render_monte_interface(){
                         message.innerText = error;
                     });
             }
+            modal.hideModal();
         }
 
         const inputNames = [
@@ -101,12 +103,12 @@ function render_monte_interface(){
             {Bonds: .40, Stocks: .60},
             {Bonds: .50, Stocks: .50},
             {Bonds: .60, Stocks: .40},
-            {Bonds: .70, Stocks: .30}]
+            {Bonds: .70, Stocks: .30}];
 
         let elem = document.createElement("div");
         let formParagraph = document.createElement("p");
         formParagraph.classList.add("form-style");
-        formParagraph.innerHTML = "Monte Carlo Simulation";
+        formParagraph.innerText = "New Simulation";
         elem.appendChild(formParagraph);
 
         let userData = document.createElement("user-data");
@@ -139,16 +141,17 @@ function render_monte_interface(){
             simulationDiv.appendChild(actualInput);
         })
 
-        const submitButton = document.createElement("button");
-        submitButton.id = "submitInfo";
-        submitButton.innerHTML = "Submit";
-        submitButton.addEventListener("click", submitFunction);
-        simulationDiv.appendChild(submitButton);
+        let buttonDiv = document.createElement("div");
+        buttonDiv.classList.add("button-container");
+        const submitButton = createButton({text: "Submit", type: "btn-success", onclickhandler: submitFunction});
+        const cancelButton = createButton({text: "Cancel", type: "btn-danger", onclickhandler: modal.hideModal});
+        buttonDiv.appendChild(submitButton);
+        buttonDiv.appendChild(cancelButton);
 
         let form_error = document.createElement("p");
         userData.appendChild(form_error);
         userData.appendChild(simulationDiv);
-
+        userData.appendChild(buttonDiv);
 
         elem.appendChild(userData);
         return elem;
@@ -157,6 +160,7 @@ function render_monte_interface(){
     function render_get_results() {
         const results_handler = () => {
             let status_code;
+            message.innerText = "";
             monte_api.get_results()
                 .then((response) => {
                     status_code = response.status;
@@ -173,8 +177,12 @@ function render_monte_interface(){
                         message.innerText = "No simulation results were found for this account";
                     }
                     else if (status_code === 200) {
-                        // Replace this with call to draw the chart
-                        message.innerText = "Got results";
+                        message.innerText = "";
+                        // have to remove any chart on the page first
+                        if(monte_vis.chart){
+                            monte_vis.chart.destroy();
+                        }
+                        monte_vis.future_value_chart(data.results.future_values);
                     }
                     else{
                         throw Error(data);
@@ -186,259 +194,47 @@ function render_monte_interface(){
                 });
         }
 
-        let elem = document.createElement("div");
-        elem.classList.add("monte-carlo-results");
         let get_button = createButton({text: "Get Results", onclickhandler: results_handler});
-        elem.appendChild(get_button);
 
         results_handler();
-        return elem
+        return get_button;
     }
 
 
-
-    let container = document.createElement("div");
-    container.classList.add("monte-carlo-interface");
-    let message = document.createElement("p");
-    let results_div = render_get_results();
+    let modal = modalFW({className: "modal-monte"});
     let form_div = render_monte_form();
-
-    container.appendChild(message);
-    container.appendChild(results_div);
-    container.appendChild(form_div);
+    let container = document.createElement("div");
+    container.classList.add("monte-container");
+    
+    let header = document.createElement("h3");
+    header.innerText = "Monte Carlo Simulation";
+    
+    let interface_items = document.createElement("div");
+    interface_items.classList.add("monte-carlo-interface");
+    let instructions = document.createElement("p");
+    instructions.innerText = "Anything you need to know about the simulation";
+    let controls = document.createElement("div");
+    let message = document.createElement("p");
+    let get_button = render_get_results();
+    let sim_button = createButton({
+        text: "New Simulation",
+        type: "btn-success",
+        onclickhandler: ()=>{
+            modal.renderForm(form_div);
+        }
+    });
+    controls.appendChild(get_button);
+    controls.appendChild(sim_button);
+    controls.appendChild(message);
+    
+    interface_items.appendChild(instructions);
+    interface_items.appendChild(controls);
+    
+    container.appendChild(modal);
+    container.appendChild(header);
+    container.appendChild(interface_items);
+    
 
     return container
 }
-/**
- * Helper function for Charting that calculates the difference between the start date and the days that the portfolio's value was updated.
- * @param {Array} dateData Array of Dates for a Portfolio
- * @returns {Array} Number of Days since the start of the Portfolio
- */
-function calculate_days_after(dateData) {
-    const start_date = new Date(dateData[0]);
-    let daysAfter = [];
 
-    for (let i = 0; i < dateData.length; i++) {
-        let difference = days_after_update(new Date(dateData[i]), start_date);
-        daysAfter.push(difference);
-    }
-
-    return daysAfter;
-}
-
-Array.prototype.createHistogram = function (numOfBins) {
-    const obj = []
-
-    const low = Math.min(...this)
-    const high = Math.max(...this)
-    const range = high - low;
-
-    console.log(low, high, range)
-
-    for (i = 0; i < numOfBins; i++) {
-        const start = i === 0 ? low : low + Math.floor(i * range / numOfBins) + 1
-
-        const end = i === numOfBins ? high : low + Math.ceil((i + 1) * range / numOfBins)
-
-        obj.push({
-            label: `${start} - ${end}`,
-            val: this.filter(a => (a >= start && a <= end)).length,
-        })
-    }
-
-    return obj
-}
-
-
-/**
- * Helper function that parses dates in 'YYYY-MM-DD' string format to a date object
- * @param dateString
- * @returns {Date}
- */
-function parseDate(dateString) {
-    let arr = dateString.split('-');
-    let year = parseInt(arr[0]);
-    let month = parseInt(arr[1]) - 1;
-    let day = parseInt(arr[2]);
-
-    return new Date(year, month, day);
-}
-
-function future_value_chart() {
-
-    const JSON_OBJ = {
-        "id": 1,
-        "user": 13,
-        "results": {
-            "future_values": [
-                1046.56,
-                1154.94,
-                1542.59,
-                1387.89,
-                1196.59,
-                1170.66,
-                1126.76,
-                1317.22,
-                1304.45,
-                1034.08,
-                1134.68,
-                1463.98,
-                1426.3,
-                1061.88,
-                1063.89,
-                1227.6,
-                914.21,
-                1407.47,
-                1785.84,
-                1372.43,
-                1102.14,
-                1019.35,
-                1453.75,
-                1129.94,
-                1050.23,
-                1022.35,
-                1285.19,
-                989.67,
-                1293.87,
-                1044.2,
-                1145.93,
-                1180.85,
-                1233.69,
-                957.45,
-                1197.92,
-                938.91,
-                922.07,
-                1342.5,
-                1093.55,
-                1131.41,
-                978.18,
-                1264.0,
-                1072.6,
-                992.54,
-                1305.09,
-                1341.14,
-                883.39,
-                1279.41,
-                1041.69,
-                816.95,
-                1281.63,
-                1440.59,
-                1104.21,
-                1223.67,
-                890.39,
-                1106.15,
-                1378.08,
-                1124.14,
-                1174.4,
-                1526.44,
-                1312.38,
-                1081.32,
-                1083.36,
-                1062.83,
-                1659.96,
-                1227.95,
-                1369.67,
-                1016.39,
-                938.22,
-                1578.99,
-                1125.93,
-                1279.15,
-                1343.06,
-                1435.16,
-                1311.89,
-                1306.03,
-                1610.26,
-                945.09,
-                923.05,
-                1209.17,
-                1083.22,
-                1583.71,
-                1194.65,
-                1133.85,
-                1117.75,
-                1432.3,
-                998.02,
-                1143.44,
-                1039.65,
-                1706.64,
-                982.13,
-                1006.56,
-                1316.14,
-                1855.39,
-                1176.8,
-                1652.08,
-                1494.07,
-                1340.12,
-                1326.41,
-                1055.55,
-                1286.75
-            ]
-        },
-        "date": "2021-04-03T14:40:46.314997Z"
-    }
-
-    Array.prototype.createHistogram = function (numOfBins) {
-
-
-        const obj = []
-
-        const low = Math.min(...this)
-        const high = Math.max(...this)
-        const range = high - low;
-        let lastStart = null;
-
-        for (i = 0; i < numOfBins; i++) {
-            const start = lastStart || Math.floor((i === 0 ? low : low + Math.floor(i * range / numOfBins) + 1) / 100) * 100
-
-
-            const end = Math.ceil((i === numOfBins ? high : low + Math.ceil((i + 1) * range / numOfBins)) / 100) * 100
-
-            lastStart = end;
-
-
-            obj.push({
-                label: `${start}`,
-                val: this.filter(a => (a > start && a < end)).length,
-            })
-        }
-
-        return obj
-    }
-
-    Array.prototype.bellCurve = function () {
-        return this.sort((a, b) => a.val % 2 - b.val % 2 || (a.val % 2 ? b.val - a.val : a.val - b.val))
-    }
-
-    const histo = JSON_OBJ.results.future_values.createHistogram(10)
-
-
-    var labels = histo.map(function (e) {
-        return e.label;
-    });
-
-    var data = histo.map(function (e) {
-        return e.val;
-    });
-
-    var ctx = document.getElementById("myChart");
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        responsive: true,
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Jason',
-                fill: false,
-                data: data,
-            }]
-        }
-    })
-    /* range = Math.max(array) - Math.min(array);
-    bucket_range = range/numBuckets;
-    for(i in array.length){
-      index = Math.floor(array[i]  / bucket_range);
-      new_array[index]++;
-    } */
-
-
-}
---
+++ fiscal/static/js/Planning/monte_carlo_visualization.js
@@ -0,0 +1,123 @@
+/**
+ * Helper function for Charting that calculates the difference between the start date and the days that the portfolio's value was updated.
+ * @param {Array} dateData Array of Dates for a Portfolio
+ * @returns {Array} Number of Days since the start of the Portfolio
+ */
+function calculate_days_after(dateData) {
+    const start_date = new Date(dateData[0]);
+    let daysAfter = [];
+
+    for (let i = 0; i < dateData.length; i++) {
+        let difference = days_after_update(new Date(dateData[i]), start_date);
+        daysAfter.push(difference);
+    }
+
+    return daysAfter;
+}
+
+Array.prototype.createHistogram = function (numOfBins) {
+    const obj = []
+
+    const low = Math.min(...this)
+    const high = Math.max(...this)
+    const range = high - low;
+
+    console.log(low, high, range)
+
+    for (i = 0; i < numOfBins; i++) {
+        const start = i === 0 ? low : low + Math.floor(i * range / numOfBins) + 1
+
+        const end = i === numOfBins ? high : low + Math.ceil((i + 1) * range / numOfBins)
+
+        obj.push({
+            label: `${start} - ${end}`,
+            val: this.filter(a => (a >= start && a <= end)).length,
+        })
+    }
+
+    return obj
+}
+
+
+/**
+ * Helper function that parses dates in 'YYYY-MM-DD' string format to a date object
+ * @param dateString
+ * @returns {Date}
+ */
+function parseDate(dateString) {
+    let arr = dateString.split('-');
+    let year = parseInt(arr[0]);
+    let month = parseInt(arr[1]) - 1;
+    let day = parseInt(arr[2]);
+
+    return new Date(year, month, day);
+}
+
+let monte_vis = {};
+monte_vis.future_value_chart = function (array) {
+
+    Array.prototype.createHistogram = function (numOfBins) {
+
+
+        const obj = []
+
+        const low = Math.min(...this)
+        const high = Math.max(...this)
+        const range = high - low;
+        let lastStart = null;
+
+        for (i = 0; i < numOfBins; i++) {
+            const start = lastStart || Math.floor((i === 0 ? low : low + Math.floor(i * range / numOfBins) + 1) / 100) * 100
+
+
+            const end = Math.ceil((i === numOfBins ? high : low + Math.ceil((i + 1) * range / numOfBins)) / 100) * 100
+
+            lastStart = end;
+
+
+            obj.push({
+                label: `${start}`,
+                val: this.filter(a => (a > start && a < end)).length,
+            })
+        }
+
+        return obj
+    }
+
+    Array.prototype.bellCurve = function () {
+        return this.sort((a, b) => a.val % 2 - b.val % 2 || (a.val % 2 ? b.val - a.val : a.val - b.val))
+    }
+
+    const histo = array.createHistogram(20)
+
+
+    var labels = histo.map(function (e) {
+        return e.label;
+    });
+
+    var data = histo.map(function (e) {
+        return e.val;
+    });
+
+    var ctx = document.getElementById("myChart");
+    monte_vis.chart = new Chart(ctx, {
+        type: 'bar',
+        responsive: true,
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Jason',
+                fill: false,
+                data: data,
+            }]
+        }
+    })
+
+    // Used to remove chart from page when user navigates away
+    function removeChart() {
+            monte_vis.chart.destroy();
+        }
+
+    window.addEventListener('hashchange', removeChart);
+
+}
