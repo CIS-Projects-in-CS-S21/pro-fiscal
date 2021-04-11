612043a8cfe433c301c459c54cd3304129090712
Jsndng408 <tuh22928@temple.edu>
2021-04-11 14:05:40 -0400

    More unit testing for Wealth Target

--- fiscal/static/__tests__/wealth_target_calculator.spec.js
+++ fiscal/static/__tests__/wealth_target_calculator.spec.js
@@ -59,4 +59,72 @@ describe("Checking for proper calculations of the Wealth Target using Compound I
 
         expect(actual).toBeUndefined();
     });
+
+    test("This should ensure that we avoid divide by zero errors.", () => {
+        const input = {
+            "id": 1,
+            "user": 13,
+            "wealth_target_inputs": {
+                "target_wealth": 14000,
+                "annual_return": -1,
+                "num_years": 20
+            },
+            "date": "2021-04-03T14:40:46.314997Z"
+        };
+
+        let actual = wealth_target_calculator(input);
+
+        expect(actual).toBeNull();
+    });
+
+    test("This should ensure that we take care of any non-numeric inputs.", () => {
+        const input = {
+            "id": 1,
+            "user": 13,
+            "wealth_target_inputs": {
+                "target_wealth": "Fail",
+                "annual_return": 0.05,
+                "num_years": 20
+            },
+            "date": "2021-04-03T14:40:46.314997Z"
+        };
+
+        let actual = wealth_target_calculator(input);
+
+        expect(actual).toBeNull();
+    });
+
+    test("This should ensure that we avoid issues with too many decimal places for future value.", () => {
+        const input = {
+            "id": 1,
+            "user": 13,
+            "wealth_target_inputs": {
+                "target_wealth": 1400.555,
+                "annual_return": 0.05,
+                "num_years": 20
+            },
+            "date": "2021-04-03T14:40:46.314997Z"
+        };
+
+        let actual = wealth_target_calculator(input);
+
+        expect(actual).toBeNull();
+    });
+
+    test("This should ensure that we avoid issues with negative number inputs.", () => {
+        const input = {
+            "id": 1,
+            "user": 13,
+            "wealth_target_inputs": {
+                "target_wealth": 1441.55,
+                "annual_return": 0.05,
+                "num_years": -10
+            },
+            "date": "2021-04-03T14:40:46.314997Z"
+        };
+
+        let actual = wealth_target_calculator(input);
+
+        expect(actual).toBeNull();
+    });
 });
