8f2e79ebcc10fe2e379919eeeab9d2b1cd5eee3b
Jsndng408 <tuh22928@temple.edu>
2021-04-11 13:36:02 -0400

    More unit testing for Wealth Target Calculator

--- fiscal/static/__tests__/wealth_target_calculator.spec.js
+++ fiscal/static/__tests__/wealth_target_calculator.spec.js
@@ -1,6 +1,6 @@
 const wealth_target_calculator = require("../js/Planning/calculate_target");
 
-describe("Checking for days after", () => {
+describe("Checking for proper calculations of the Wealth Target using Compound Interest Formula.", () => {
     test("This should determine whether the wealth target calculator correctly calculates the right value.", () => {
         const input = {
             "id": 1,
@@ -23,5 +23,40 @@ describe("Checking for days after", () => {
         });
 
         expect(formatter.format(actual)).toEqual(output);
+    });
+
+    test("This should test whether the formatter will display the decimal as USD properly.", () => {
+        const input = {
+            "id": 1,
+            "user": 13,
+            "wealth_target_inputs": {
+                "target_wealth": 14000,
+                "annual_return": 0.1,
+                "num_years": 20
+            },
+            "date": "2021-04-03T14:40:46.314997Z"
+        };
+
+        const output = "$2,081.01";
+
+        let actual = wealth_target_calculator(input);
+
+        expect(String(actual)).not.toBe(output);
+    });
+
+    test("This should ensure that you get undefined if you do not pass in inputs for all three required inputs.", () => {
+        const input = {
+            "id": 1,
+            "user": 13,
+            "wealth_target_inputs": {
+                "target_wealth": 14000,
+                "annual_return": 0.1
+            },
+            "date": "2021-04-03T14:40:46.314997Z"
+        };
+
+        let actual = wealth_target_calculator(input);
+
+        expect(actual).toBeUndefined();
     });
 });
--- fiscal/static/js/Planning/calculate_target.js
+++ fiscal/static/js/Planning/calculate_target.js
@@ -3,6 +3,10 @@ function wealth_target_calculate(params) {
     let annual_return = params.wealth_target_inputs.annual_return;
     let num_years = params.wealth_target_inputs.num_years;
 
+    if (target_wealth === undefined || annual_return === undefined || num_years === undefined) {
+        return undefined;
+    }
+
     const finalValue = target_wealth / ((1 + annual_return) ** num_years);
 
     return finalValue;
