14e24cc459479321e37cdcf1bef43a7381639693
Benjamin Levin <blevin@temple.edu>
2021-04-11 09:36:19 -0400

    classification phrases now passed at run time

--- machine_learning/model/class_test.py
+++ machine_learning/model/class_test.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 import pickle
+import sys
 
 from nltk.tokenize import word_tokenize
 from nltk import pos_tag
@@ -15,19 +16,34 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import svm
 
 def main():
-    #Import pickled models
-    clf = pickle.load(open('linear_clf.pkl', 'rb'))
-    Tfidf_vect = pickle.load(open('Tfidf_vect.pkl', 'rb'))
+    # verify that a description has been passed: `python class_test.py 'morgage payment'`
+    if (len(sys.argv) < 2):
+        print('Error: please enter description to classify')
+    elif (len(sys.argv) == 2):
+        #description entered in ""
+        desc = str(sys.argv[1])
+        print_result(desc)
+    else:
+        #description entered as list of args: `python class_test.py morgage payment`
+        desc = ' '.join(sys.argv[1:])
+        print_result(desc)
 
+def make_tag_map():
     #create tag map for word pre-processing
     tag_map = defaultdict(lambda : wn.NOUN)
     tag_map['J'] = wn.ADJ
     tag_map['V'] = wn.VERB
     tag_map['R'] = wn.ADV
-
-    print_result('Food from Stop & Shop')
+    return tag_map
 
 def get_cat(cat_desc):
+    #Import pickled models
+    clf = pickle.load(open('linear_clf.pkl', 'rb'))
+    Tfidf_vect = pickle.load(open('Tfidf_vect.pkl', 'rb'))
+
+    #create tag map for word pre-processing
+    tag_map = make_tag_map()
+
     #list of the category names
     cat_list = ['Housing', 'Transportation', 'Debt', 'Insurance',
             'Utilities', 'Medical/Healthcare', 'Savings', 'Retirement',
