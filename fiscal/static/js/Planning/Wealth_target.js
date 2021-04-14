edb4e602e4ba70ba97c6eb8b7904610f2543097a
Jsndng408 <tuh22928@temple.edu>
2021-04-14 18:59:51 -0400

    Refactoring Wealth Target Calculator interface to have result stand out

--- fiscal/static/js/Planning/Wealth_target.js
+++ fiscal/static/js/Planning/Wealth_target.js
@@ -37,8 +37,7 @@ function render_wealthTarget_interface() {
 
         let results = wealth_target_calculate(params);
 
-        let item = document.getElementById("presentValue");
-        item.value = formatter.format(results);
+        outputAnswer.innerText = formatter.format(results);
     }
 
     const inputContent = [
@@ -48,7 +47,7 @@ function render_wealthTarget_interface() {
                 if (isNaN(e.target.value) || e.target.value.length > 7) {
                     e.target.value = e.target.value.slice(0, -1)
                 }
-            }, type: "text", inputId: "TotalIncome", disabled: false
+            }, type: "text", inputId: "TotalIncome"
         },
         {
             inputText: "Enter the interest rate at the given time period (r): ",
@@ -57,7 +56,7 @@ function render_wealthTarget_interface() {
                     e.target.value = e.target.value.slice(0, -1)
                 }
             }
-            , type: "text", inputId: "Rate", disabled: false
+            , type: "text", inputId: "Rate"
         },
         {
             inputText: "Enter the number of years that you have before you can retire (n): ",
@@ -66,14 +65,7 @@ function render_wealthTarget_interface() {
                     e.target.value = e.target.value.slice(0, -1)
                 }
             }
-            , type: "text", inputId: "time", disabled: false
-        },
-        {
-            inputText: "Present Value to save to attain your Future Value (P): ",
-            actualFunction: (e) => {
-
-            }
-            , type: "text", inputId: "presentValue", disabled: true
+            , type: "text", inputId: "time"
         }
     ];
 
@@ -92,8 +84,7 @@ function render_wealthTarget_interface() {
     `
     FormWealth.appendChild(formParagraph);
 
-
-    let userInfo = document.createElement("user-info");
+    let userInfo = document.createElement("div");
     userInfo.classList.add("wealth-inputs");
     let wealthDiv = document.createElement("div");
     wealthDiv.id = "wealth";
@@ -103,7 +94,6 @@ function render_wealthTarget_interface() {
         actual.id = input.inputId;
         actual.type = input.type;
         actual.addEventListener("input", input.actualFunction);
-        actual.disabled = input.disabled;
         wealthDiv.appendChild(label);
         wealthDiv.appendChild(actual);
     });
@@ -116,9 +106,35 @@ function render_wealthTarget_interface() {
     submitIcon.addEventListener("click", submitWealth);
     wealthDiv.appendChild(submitIcon);
 
+    let outputBox = document.createElement("div");
+    outputBox.classList.add("wealthResult");
+
+    let outputTitle = document.createElement("h3");
+    outputTitle.innerText = "Wealth Calculator Results";
+    outputTitle.classList.add("text-center");
+    outputBox.appendChild(outputTitle);
+
+    let outputBoxDiv = document.createElement("div");
+    outputBoxDiv.classList.add("row");
+
+    let outputLabel = document.createElement("div");
+    outputLabel.innerText = "Present Value to save to attain your Future Value (P)";
+    outputLabel.classList.add("col", "text-center");
+
+    let outputAnswer = document.createElement("div");
+    outputAnswer.innerText = "$0.00";
+    outputAnswer.classList.add("col", "text-center", "wealthAnswer");
+
+    outputBoxDiv.appendChild(outputLabel);
+    outputBoxDiv.appendChild(outputAnswer);
+
+    outputBox.appendChild(outputBoxDiv);
+
     userInfo.appendChild(wealthDiv);
 
     FormWealth.appendChild(userInfo);
+    FormWealth.appendChild(document.createElement("br"));
+    FormWealth.appendChild(outputBox);
 
     return FormWealth;
 }
